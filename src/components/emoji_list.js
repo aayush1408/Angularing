5017ba144d4231a7ca1b74f41d2a053622e477b3
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-19 14:24:13 +0530

    Search enabled

--- src/components/emoji_list.js
+++ src/components/emoji_list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+
 export default class EmojiList extends React.Component{
     constructor(props){
       super(props);
@@ -12,18 +13,29 @@ export default class EmojiList extends React.Component{
           console.log(err.response);
         })
     }
+   
 
     renderList(){
+
       const {emojis}  = this.state;
-      console.log(emojis);
-        return emojis.map((emoji,i)=>{
-        return (<li key={i} className="list-group-item">{emoji.symbol} -- {emoji.title}</li>);
-      })
-    }
+      const {search} = this.props;
+      if(search === ''){
+         return emojis.map((emoji,i)=>{
+         return (<li key={i} className="list-group-item">{emoji.symbol} -- {emoji.title}</li>);
+      });
+      }
+      let items = emojis.filter((emoji)=>{
+          return emoji.title === search;
+      });
+        return items.map((emoji,i)=>{
+         return (<li key={i} className="list-group-item">{emoji.symbol} -- {emoji.title}</li>);
+      });
+      }
+    
 
     render(){
         return(
-            <div>   
+            <div className="container">   
               <ul className="list-group">
                   {this.renderList()}
               </ul>
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import EmojiList from './emoji_list';
 export default class SearchBaar extends React.Component{
   onInputChange(e){
     this.setState({term:e.target.value});
-    console.log(this.state.term);
+    // console.log(this.state.term);
       }
   constructor(props){
     super(props);
@@ -13,11 +13,13 @@ export default class SearchBaar extends React.Component{
         return(
                <div className= "container">
                 <div className="row">
-                   <input type="text" 
+                   <input 
+                   type="text" 
                    className = "form-control"
                    value={this.state.term}  
                    placeholder="Enter the description" 
                    onChange={this.onInputChange.bind(this)} />
+                   <EmojiList search={this.state.term} />
                 </div>
                </div>
                )
